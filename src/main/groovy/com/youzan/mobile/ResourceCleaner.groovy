525b7645ee6d0928052323399af93fa3426b559a
liangfei <njliangfei@gmail.com>
2016-08-12 13:02:04 +0800

    init the project; thanks Jiajixin

---
+++ src/main/groovy/com/youzan/mobile/ResourceCleaner.groovy
@@ -0,0 +1,15 @@
+package com.youzan.mobile
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Created by liangfei on 8/12/16.
+ */
+class ResourceCleanerPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
